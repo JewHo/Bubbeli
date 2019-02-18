a739cd3f3949374da1fdcf5031a7259ad9f1ba05
JewHo <43783583+JewHo@users.noreply.github.com>
2019-02-18 22:32:43 +0200

    navbar spotify etc.

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,28 @@
+$("#scrolla a[href^='#']").on('click', function(e) {
+
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // store hash
+     var hash = this.hash;
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 1000, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = hash;
+       });
+
+  });
+
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
 $(function() {
 var selectedClass = "";
 $(".filter").click(function(){
