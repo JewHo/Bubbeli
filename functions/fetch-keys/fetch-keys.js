bc87f6f82b2ff3158d1b2a510fecbcb8090b245e
Juho <juho.a.rinne@gmail.com>
2022-11-25 00:22:52 +1100

    Fix nr 10000

--- functions/fetch-keys/fetch-keys.js
+++ functions/fetch-keys/fetch-keys.js
@@ -1,33 +1,33 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-import fetch from "node-fetch";
-
-const handler = async (event) => {
-
-    var client_id = process.env.MY_KEY;
-    var client_secret = process.env.SECRET_KEY; // Your secret
 
 
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("grant_type", "client_credentials");
+const API_ENDPOINT = 'https://accounts.spotify.com/api/token';
+var client_id = process.env.MY_KEY;
+var client_secret = process.env.SECRET_KEY; // Your secret
 
-    const requestOptions = {
-            method: 'POST',
-            headers: {
-              'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
-              'Content-Type': "application/x-www-form-urlencoded"
-            },
-            body: urlencoded,
-            redirect: 'follow'
-          }
-    try {
-    let res = await fetch("https://accounts.spotify.com/api/token", requestOptions);
-    res = await res.json();
-    console.log(res);
-    return { statusCode: 200, body: JSON.stringify(res) }
+var urlencoded = new URLSearchParams();
+urlencoded.append("grant_type", "client_credentials");
 
+const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+          'Content-Type': "application/x-www-form-urlencoded"
+        },
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(API_ENDPOINT, requestOptions);
+    const data = await response.json();
+    return { statusCode: 200, body: JSON.stringify(data) };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
   }
-}
-
-module.exports = { handler }
+};
