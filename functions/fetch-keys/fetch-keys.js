4068c5eff39f13d13d72306b1cd28aa1fb514e6b
Juho <juho.a.rinne@gmail.com>
2022-11-26 21:42:36 +1100

    troubleshooting logging client_id

--- functions/fetch-keys/fetch-keys.js
+++ functions/fetch-keys/fetch-keys.js
@@ -6,31 +6,30 @@ import fetch from "node-fetch";
 
 const handler = async (event, context) => {
 
-  console.log('Helo wrld');
+  //console.log('Helo wrld');
   const API_ENDPOINT = 'https://accounts.spotify.com/api/token';
   var client_id = process.env.MY_KEY;
   var client_secret = process.env.SECRET_KEY; // Your secret
-  console.log(client_id);
+  //console.log(client_id);
 
-  var urlencoded = new URLSearchParams();
-  urlencoded.append("grant_type", "client_credentials");
-  console.log(urlencoded);
+  //var urlencoded = new URLSearchParams();
+  //urlencoded.append("grant_type", "client_credentials");
+  //console.log(urlencoded);
 
-  const requestOptions = {
-          method: 'POST',
-          headers: {
-            'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
-            'Content-Type': "application/x-www-form-urlencoded"
-          },
-          body: urlencoded,
-          redirect: 'follow'
-        }
-  console.log(requestOptions);
+  //const requestOptions = {
+    //      method: 'POST',
+      //    headers: {
+        //    'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+        //    'Content-Type': "application/x-www-form-urlencoded"
+        //  },
+        //  body: urlencoded,
+        //  redirect: 'follow'
+    //    }
+  //console.log(requestOptions);
   try {
 
-    const response = await fetch(API_ENDPOINT, requestOptions);
-    const data = await response.json();
-    return { statusCode: 200, body: JSON.stringify(data) };
+    //const response = await fetch(API_ENDPOINT, requestOptions).then(res => res.json());
+    return { statusCode: 200, body: JSON.stringify(client_id) };
     //return { statusCode: 200, body: JSON.stringify({ client_id }) };
   } catch (error) {
     console.log(error);
